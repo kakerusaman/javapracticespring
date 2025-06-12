bb8b41c6ad192f32a5958cef2294f6dcc577aa78
kakerusaman <kakerusaman@gmail.com>
2025-06-12 23:20:00 +0900

    configurationアノテーションを追加

--- src/main/java/com/example/practice/config/AppConfig.java
+++ src/main/java/com/example/practice/config/AppConfig.java
@@ -1,5 +1,14 @@
 package com.example.practice.config;
 
+import org.springframework.context.annotation.Configuration;
+
+/*
+@Configurationはそのクラスが設定クラスであることを示しています。
+このクラスの中で@Beanアノテーションなどをつかい、Springコンテナ（DIコンテナ）に登録したいオブジェクト（Bean）の生成や記述方法を定義します。
+@ConfigurationがついたクラスはSpringによって実行時に読み込まれてアプリケーションの設定や依存性の注入を行います。
+*/ 
+
+@Configuration
 public class AppConfig {
     
 }
